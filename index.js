c28a8e0b748afea6f15e5c34b0ff71750ffb7b58
Franck <franck.abgrall@zenika.com>
2018-10-31 16:41:03 +0100

    :tada: Init commit

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const chalk = require('chalk')
 const service = require('./service')
 
 console.log(chalk.green(`${service.sayHello()} ${service.sayWorld()}`))
+
+
+service.log('Hello world\n')
--- service.js
+++ service.js
@@ -1,2 +1,6 @@
+const fs = require('fs').promises
+
 exports.sayHello = () => 'Hello'
 exports.sayWorld = () => 'World'
+
+exports.log = dataString => fs.appendFile('./out.log', dataString)
